946814c34ef9348d450f0a998a71b40cd1874919
Nick Spain <nicholas.spain96@gmail.com>
2018-09-26 23:05:28 +1000

    Cleanup tests

--- app/src/androidTest/java/xyz/nspain/grocerybudget/utils/RecyclerViewUtils.java
+++ app/src/androidTest/java/xyz/nspain/grocerybudget/utils/RecyclerViewUtils.java
@@ -1,4 +1,4 @@
-package xyz.nspain.grocerybudget;
+package xyz.nspain.grocerybudget.utils;
 
 import android.content.res.Resources;
 import android.support.annotation.IdRes;
@@ -15,16 +15,29 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.TestCase.fail;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class Utils {
+/**
+ * Utilities for instrumentation tests.
+ */
+public class RecyclerViewUtils {
 
+    /**
+     * Get an Espresso matcher for a given RecyclerView to use for testing.
+     *
+     * @param id Resource ID to use
+     * @return Espresso matcher for the given RecyclerView
+     */
     public static RecyclerViewMatcher withRecyclerView(final int id) {
         return new RecyclerViewMatcher(id);
     }
 
+    /**
+     * Espresso assertion about the the number of items in a RecyclerView.
+     */
     public static class RecyclerViewItemCountAssertion implements ViewAssertion {
         private int assertedCount;
 
@@ -37,26 +50,45 @@ public class Utils {
             if (noViewFoundException != null) {
                 throw noViewFoundException;
             }
-            RecyclerView recyclerView = (RecyclerView)view;
+            RecyclerView recyclerView = (RecyclerView) view;
             RecyclerView.Adapter adapter = recyclerView.getAdapter();
-            assertThat(adapter.getItemCount(), is(assertedCount));
+            if (adapter != null) {
+                assertThat(adapter.getItemCount(), is(assertedCount));
+            } else {
+                fail("Recycler view does not have an adapter");
+            }
         }
     }
+
     /**
+     * Espresso matcher for a RecyclerView
      * From https://stackoverflow.com/questions/28476507/using-espresso-to-click-view-inside-recyclerview-item
      */
     public static class RecyclerViewMatcher {
         private final int recyclerViewId;
 
-        public RecyclerViewMatcher(int recyclerViewId) {
+        public RecyclerViewMatcher(@IdRes int recyclerViewId) {
             this.recyclerViewId = recyclerViewId;
         }
 
+        /**
+         * Match an item in the RecyclerView in a given position.
+         *
+         * @param position Position of item in RecyclerView
+         * @return Espresso matcher for item
+         */
         public Matcher<View> atPosition(final int position) {
             return atPositionOnView(position, -1);
         }
 
-        public Matcher<View> atPositionOnView(final int position, final int targetViewId) {
+        /**
+         * Get an Espresso matcher for a specific view of a RecyclerView item.
+         *
+         * @param position     Index of item in RecyclerView
+         * @param targetViewId Resource ID of view within item of RecyclerView
+         * @return Espresso matcher for item with the given resource ID
+         */
+        public Matcher<View> atPositionOnView(final int position, @IdRes final int targetViewId) {
             return new TypeSafeMatcher<View>() {
                 Resources resources = null;
                 View childView;
@@ -66,7 +98,12 @@ public class Utils {
                     if (childView == null) {
                         RecyclerView recyclerView = item.getRootView().findViewById(recyclerViewId);
                         if (recyclerView != null) {
-                            childView = recyclerView.findViewHolderForAdapterPosition(position).itemView;
+                            RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForAdapterPosition(position);
+                            if (viewHolder != null) {
+                                childView = viewHolder.itemView;
+                            } else {
+                                return false;
+                            }
                         } else {
                             return false;
                         }
@@ -98,20 +135,31 @@ public class Utils {
         }
     }
 
+    /**
+     * Get the number of items in a recycler view
+     *
+     * @param RecyclerViewId Resource ID of RecyclerView to count items in
+     * @return Item count in RecyclerView
+     */
     public static int getCountFromRecyclerView(@IdRes int RecyclerViewId) {
         final int[] COUNT = {0};
-        Matcher matcher = new TypeSafeMatcher<View>() {
+        Matcher<View> matcher = new TypeSafeMatcher<View>() {
             @Override
             protected boolean matchesSafely(View item) {
-                int itemCount = ((RecyclerView) item).getAdapter().getItemCount();
-                COUNT[0] = ((RecyclerView) item).getAdapter().getItemCount();
-                return true;
+                RecyclerView.Adapter adapter = ((RecyclerView) item).getAdapter();
+                if (adapter != null) {
+                    COUNT[0] = adapter.getItemCount();
+                    return true;
+                }
+                return false;
             }
+
             @Override
             public void describeTo(Description description) {
             }
         };
-        onView(allOf(withId(RecyclerViewId),isDisplayed())).check(matches(matcher));
+
+        onView(allOf(withId(RecyclerViewId), isDisplayed())).check(matches(matcher));
         return COUNT[0];
     }
 }
